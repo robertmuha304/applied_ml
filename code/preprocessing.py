28cf13856beac0fd0cc63f64fc3fa0c0b629bcd7
Arseni Muravjov <arsenimuravjov@MacBook-Air-Arseni.local>
2025-05-09 13:14:24 +0200

    salary

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -6,14 +6,14 @@ df = pd.read_csv("fake_job_postings.csv")
 
 print(df.head())
 
-# eve
 
-# df.fillna(" ", inplace=True)
 df = df.replace(np.nan, '', regex=True)
-print(df.isnull().sum())
+
 
 edited_location = df['location'].str.split(',').str[0]
 df['location'] = edited_location.values
+
+print(df.isnull().sum())
 
 df["text"] = df["title"] + " " + df["description"] + " " + df["location"] + " " + df["department"] + " " + df["company_profile"] + " " + df[
     "requirements"] + " " + df["benefits"] + " " + df["employment_type"] + " " + df["required_experience"] + " " + df[
@@ -25,5 +25,13 @@ df["text"] = df["text"].str.replace(r'[^\w\s]', '', regex=True)
 df = df.drop(columns=["title", "description", "location", "department", "company_profile", "requirements", "benefits",
                  "employment_type", "required_experience", "required_education", "industry", "function"])
 
-print(df)
+split = df["salary_range"].str.split("-", expand=True)
+salary_min = pd.to_numeric(split[0], errors="coerce")
+salary_max = pd.to_numeric(split[1], errors="coerce")
+
+df["salary_range"] = (salary_min + salary_max) // 2
+df["salary_range"] = df["salary_range"].fillna("missing")
+print(df[:20])
+print(df.info())
+
 
